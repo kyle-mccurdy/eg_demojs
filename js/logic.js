661d6d517829a4aa2d9449b3ddd32984cc6d6645
Eric J York <eyork@clarkson.edu>
2025-04-03 11:10:00 -0500

    adds scripts done in class

--- js/logic.js
+++ js/logic.js
@@ -1,32 +1,32 @@
-// function flipCoin(x) {
-//     let pick = x; // pick is set to the passed-in parameter (from html)
-//     let choice = document.getElementById('choice'); // to show player choice
-//     let flip = document.getElementById('flip'); // to show flip result
-//     let result = document.getElementById('result'); // to show game result
+function flipCoin(x) {
+    let pick = x; // pick is set to the passed-in parameter (from html)
+    let choice = document.getElementById('choice'); // to show player choice
+    let flip = document.getElementById('flip'); // to show flip result
+    let result = document.getElementById('result'); // to show game result
 
-//     // Determines the face of the toss
-//     let randNum = Math.ceil(Math.random() * 2); // randomly a 1 or 2
-//     let toss; // to hold the toss result
+    // Determines the face of the toss
+    let randNum = Math.ceil(Math.random() * 2); // randomly a 1 or 2
+    let toss; // to hold the toss result
 
-//     if (randNum === 1) {
-//         toss = 'heads';
-//     } else {
-//         toss = 'tails';
-//     }
+    if (randNum === 1) {
+        toss = 'Heads';
+    } else {
+        toss = 'Tails';
+    }
 
-//     // determines the result of guess
-//     let status; // to hold the result of the guess
-//     if (pick === toss ) {
-//         status = "won";
-//     } else {
-//         status = 'lost';
-//     }
+    // determines the result of guess
+    let status; // to hold the result of the guess
+    if (pick === toss ) {
+        status = "Won";
+    } else {
+        status = 'Lost';
+    }
 
-//     // read-outs of all the results
-//     choice.innerText = pick;
-//     flip.innerText = toss;
-//     result.innerText = status;
-// }
+    // read-outs of all the results
+    choice.innerText = pick;
+    flip.innerText = toss;
+    result.innerText = status;
+}
 
 // // Shows how to pass a paremeter
 // function flipCoin(pick) {
@@ -39,9 +39,9 @@
 //     let randNum = Math.floor(Math.random() * 2) + 1;
 //     let toss;
 //     if (randNum == 1) {
-//         toss = 'tails';
+//         toss = 'Tails';
 //     } else {
-//         toss = 'heads';
+//         toss = 'Heads';
 //     }
 //     display.innerText = toss;
 //     console.log(randNum);
@@ -66,4 +66,36 @@
     
 //     // logs randNum in the console (browser inspector)
 //     console.log(randNum);
+// }
+
+// function flipCoin(x) {
+
+//     let pick = x;
+
+//     let result = document.getElementById('flip');
+//     let choice = document.getElementById('choice');
+//     let game = document.getElementById('result');
+
+//     let randNum = Math.ceil(Math.random() * 2);
+
+//     let face;
+//     if (randNum == 1) {
+//         face = "Heads";
+//     } else {
+//         face = "Tails";
+//     }
+
+//     let status;
+//     if (face == choice) {
+//         status = "Win";
+//     } else {
+//         status = "Loss";
+//     }
+
+//     result.innerText = face;
+//     choice.innerText = pick;
+//     game.innerText = status;
+
+//     console.log(randNum);
+
 // }
